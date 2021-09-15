f2a564f175347c69972c5524398f81c56a21baaf
cerdemrc <cerdemrc@gmail.com>
2021-09-16 02:01:29 +0300

    fix button position

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="tracker-header">
     <h1 class="tracker-header-title main-title">IP Adress Tracker</h1>
-    <input
-      type="text"
-      placeholder="Search for any IP address or domain"
-      v-model="searchText"
-      @keydown.enter="sendSearchText"
-    />
-    <button class="btn btn-dark" @click="sendSearchText">
-      <img src="../../images/icon-arrow.svg" />
-    </button>
+    <div class="tracker-header-input">
+      <input
+        type="text"
+        placeholder="Search for any IP address or domain"
+        v-model="searchText"
+        @keydown.enter="sendSearchText"
+      />
+      <a href="#" class="btn" @click="sendSearchText">
+        <img src="../../images/icon-arrow.svg" />
+      </a>
+      <!--<button class="btn btn-dark" @click="sendSearchText">
+        <img src="../../images/icon-arrow.svg" />
+      </button>-->
+    </div>
   </div>
 </template>
 
@@ -37,35 +42,68 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  position: relative;
+  &-input {
+    position: relative;
+    & input {
+      margin-top: 2.5rem;
+      padding: 18px;
+      width: 600px;
+      outline: none;
+      border-radius: 16px;
+      border: none;
+      &::placeholder {
+        font-size: 1.7rem;
+        font-family: "Rubik", sans-serif;
+        color: $lightGray;
+      }
+    }
+    & .btn {
+      background: #000;
+      padding: 19px 20px;
+      border-radius: 0 16px 16px 0;
+      position: absolute;
+      top: 25px;
+      right: 0px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
   &-title {
     margin-top: 5rem;
     font-size: 4rem;
     color: $white;
   }
-  & input {
-    margin-top: 2.5rem;
-    padding: 18px;
-    width: 600px;
-    outline: none;
-    border-radius: 16px;
-    border: none;
-    &::placeholder {
-      font-size: 1.7rem;
-      font-family: "Rubik", sans-serif;
-      color: $lightGray;
+}
+
+@media screen and (max-width: 610px) {
+  .tracker-header {
+    &-title {
+      font-size: 3rem;
+    }
+    & input {
+      width: 400px;
     }
   }
-  & .btn {
-    background: #000;
-    padding: 18px 20px;
-    border-radius: 0 16px 16px 0;
-    position: absolute;
-    top: 121px;
-    right: 420px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+}
+@media screen and (max-width: 400px) {
+  .tracker-header {
+    & input {
+      width: 360px;
+      &::placeholder {
+        font-size: 1.5rem;
+      }
+    }
+  }
+}
+@media screen and (max-width: 360px) {
+  .tracker-header {
+    & input {
+      width: 100%;
+      &::placeholder {
+        color: transparent;
+      }
+    }
   }
 }
 </style>
