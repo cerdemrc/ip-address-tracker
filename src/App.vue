5ee1e124eda481fe730aa08def97ab65bc9bebfa
cerdemrc <cerdemrc@gmail.com>
2021-09-16 00:18:49 +0300

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="tracker-main">
+    <div class="container">
+      <Header />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "./assets/style/_common.scss";
+@import "./assets/style/colors.scss";
+.tracker-main {
+  background: url(../images/pattern-bg.png) no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 27rem;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tracker-header">
+    <h1 class="tracker-header-title main-title">IP Adress Tracker</h1>
+    <input type="text" placeholder="Search for any IP address or domain" />
+    <button class="btn btn-dark">
+      <img src="../../images/icon-arrow.svg" />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../assets/style/_common.scss";
+@import "../assets/style/colors.scss";
+.tracker-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  &-title {
+    margin-top: 5rem;
+    font-size: 4rem;
+    color: $white;
+  }
+  & input {
+    margin-top: 2.5rem;
+    padding: 18px;
+    width: 600px;
+    outline: none;
+    border-radius: 16px;
+    border: none;
+    &::placeholder {
+      font-size: 1.7rem;
+      font-family: "Rubik", sans-serif;
+      color: $lightGray;
+    }
+  }
+  & .btn {
+    background: #000;
+    padding: 18px 20px;
+    border-radius: 0 16px 16px 0;
+    position: absolute;
+    top: 121px;
+    right: 420px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
